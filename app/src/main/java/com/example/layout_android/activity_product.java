c23168546a134196d718e96b03ddba17ca828d78
cricro168-hub <cristobal.hernan2005@gmail.com>
2025-08-24 16:47:34 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/layout_android/activity_product.java
@@ -0,0 +1,12 @@
+package com.example.layout_android;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_product extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
